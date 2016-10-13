7e5758e693a283dd0f32ccdb61ade2a49db0d86d
Matt.C <linkinpark761102@bwwtaiwan.com>
2016-10-13 17:34:12 +0800

    API message added photo URL

--- app/controllers/api_v1/auth_controller.rb
+++ app/controllers/api_v1/auth_controller.rb
@@ -10,12 +10,13 @@ class ApiV1::AuthController < ApiController
     end
  
     if success
-      render :json => { :message => "Ok～～～～～～～～",
+      render :json => { :message => "阿明你成功囉～",
                         :auth_token => user.authentication_token,
                         :user_id => user.id,
                         :first_name => user.first_name,
                         :last_name => user.last_name,
                         :phone_number => user.phone_number
+                        :photo => "https://firebasestorage.googleapis.com/v0/b/ios1-ec05d.appspot.com/o/%E6%98%93%E4%BA%9E%E5%A9%B7.png?alt=media&token=8e7b6244-45e1-41d2-ba15-7b9232f5c2a6"
                       }
      else
        render :json => { :message => "Email or Password is wrong" }, :status => 401
