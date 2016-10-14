d62e888d5338edc90002b52460d6a45b65c7059a
Matt.C <linkinpark761102@bwwtaiwan.com>
2016-10-14 18:59:08 +0800

    rescue some errors/ layout admin bug fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,24 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActionController::RoutingError, with: :not_found
+    rescue_from ActionController::UnknownController, with: :not_found
+    rescue_from ActionController::UnknownAction, with: :not_found
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  end
+
+  protected
+
+  def not_found
+    respond_to do |format|
+      format.html { render template: 'shared/errors/404', status: 404 }
+      format.json { render json: exception , status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
