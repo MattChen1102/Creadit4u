86406a20878dfd49ee38002df56163f02aa1c535
Matt.C <linkinpark761102@bwwtaiwan.com>
2016-10-14 20:15:38 +0800

    Replace query with scope in controllers

--- app/controllers/creadit4u_controller.rb
+++ app/controllers/creadit4u_controller.rb
@@ -1,7 +1,7 @@
 class Creadit4uController < ApplicationController
 	def index
-		@categories = Category.where(enabled: true).all
- 		@products = Product.order('id desc').page(params[:page])
+		@categories = Category.enabled.all
+    @products = Product.active.order('id desc').page(params[:page])
 	end
 
 	def order_list
